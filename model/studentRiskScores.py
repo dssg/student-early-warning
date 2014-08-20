6a13dacd26ce144bc07575dde0039790dc96bfe8
anfibil <anfibil@gmail.com>
2014-08-20 16:16:52 -0500

    Updated test file

--- model/studentRiskScores.py
+++ model/studentRiskScores.py
@@ -7,5 +7,7 @@ df = pd.read_csv('../data/simulated_data.csv',index_col=0)
 # Create a model object using the loaded data
 pred = classification.Model(df,'nograd',doFeatureSelection=False)
 
-# Run classification using 10-fold cross validation, logistic regression, and reporting risk scores for the top 5% of students at highest risk
+# Run classification using 10-fold cross validation
+# Classifier used: Logistic Regression (LR)
+# Output format:List of risk scores for the top 5% of students at highest risk
 pred.runClassification(outputFormat='risk', models=['LR'], topK=.05, nFolds=10)
