272ac711f5ef3f4e6ea2901b9278206ce1e4bddd
anfibil <anfibil@gmail.com>
2014-08-20 16:14:39 -0500

    Updated test file

--- model/studentRiskScores.py
+++ model/studentRiskScores.py
@@ -1,8 +1,11 @@
 import classification
 import pandas as pd
 
+# Load simulated data set for experiment
 df = pd.read_csv('../data/simulated_data.csv',index_col=0)
 
+# Create a model object using the loaded data
 pred = classification.Model(df,'nograd',doFeatureSelection=False)
 
-pred.runClassification(outputFormat='risk', doSubsampling=False, doSMOTE=True, pctSMOTE=200, models=['LR','RF'],topK=.05,nFolds=10)
+# Run classification using 10-fold cross validation, logistic regression, and reporting risk scores for the top 5% of students at highest risk
+pred.runClassification(outputFormat='risk', models=['LR'], topK=.05, nFolds=10)
