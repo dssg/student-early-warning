362cdd2b8f42a8f0da71e01d91246560c70f3ab0
anfibil <anfibil@gmail.com>
2014-08-21 11:53:36 -0500

    Fixed path to data file

--- model/studentRiskScores.py
+++ model/studentRiskScores.py
@@ -2,12 +2,12 @@ import classification
 import pandas as pd
 
 # Load simulated data set for experiment
-df = pd.read_csv('../data/simulated_data.csv',index_col=0)
+df = pd.read_csv('./data/simulated_data.csv',index_col=0)
 
 # Create a model object using the loaded data
-pred = classification.Model(df,'nograd',doFeatureSelection=True)
+pred = classification.Model(df,'nograd')
 
 # Run classification using 10-fold cross validation
 # Classifier used: Logistic Regression (LR)
 # Output format:List of risk scores for the top 5% of students at highest risk
-pred.runClassification(outputFormat='risk', models=['RF'], topK=.05, nFolds=10)
+pred.runClassification(outputFormat='risk', models=['RF'], nFolds=10)
